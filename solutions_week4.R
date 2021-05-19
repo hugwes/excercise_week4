07e2cd91063ca8c000195affd7cd498647c1c428
Wesley Hug <hugwes@students.zhaw.ch>
2021-05-19 18:19:33 +0200

    starting the excercises

---
+++ solutions_week4.R
@@ -0,0 +1,45 @@
+
+### EXCERCISE 4 ###
+
+##########################################################
+# Preparations
+# https://github.com/hugwes/excercise_week4 (github-link)
+
+##########################################################
+# Load in Libraries
+library(readr)        # to import tabular data (e.g. csv)
+library(dplyr)        # to manipulate (tabular) data
+library(ggplot2)      # to visualize data
+library(sf)           # to handle spatial vector data (sf = shape file)
+library(terra)        # to handle raster data
+library(lubridate)    # to handle dates and times
+library(zoo)          # moving window functions
+
+##########################################################
+# Task 1: Write your own functions
+
+
+##########################################################
+# Task 2: Prepare Analysis
+
+
+##########################################################
+# Task 3: Create Join Key
+
+
+##########################################################
+# Task 4: Measuring distance at concurrent locations
+
+
+##########################################################
+# Task 5: Visualize data
+
+
+##########################################################
+# Task 6: Visualize data as timecube with plotly
+
+
+
+
+
+
